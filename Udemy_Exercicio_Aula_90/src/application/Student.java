70279c70e3e4075cd55cba98747942b6e4aa1913
thoni <wanok8000@outlook.com>
2020-09-03 01:45:51 -0300

    add aula 90

---
+++ Udemy_Exercicio_Aula_90/src/application/Student.java
@@ -0,0 +1,31 @@
+package application;
+
+public class Student {
+    String name, email;
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+
+
+
+    //getter n setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
